eeff4c3a47e6c8964db5d25bfc0154e4f1938023
fgoc <feyzullahgoctr@gmail.com>
2026-01-28 16:41:12 +0300

    First commit

--- src/ugeopgave1.java
+++ src/ugeopgave1.java
@@ -11,16 +11,37 @@ void VariableBasics() {
     double firstPrice = 25.50;
     double secondPrice = 15;
     double thirdPrice = 30;
-    double subtotalen = firstPrice + secondPrice + thirdPrice;
-    double momsprocenten = 25;
-    double totalPriceVAT = subtotalen + (subtotalen * momsprocenten / 100);
-    System.out.println("subtotalen : " + subtotalen);
+    double subtotal = firstPrice + secondPrice + thirdPrice;
+    double taxRate   = 25;
+    double totalPriceVAT = subtotal + (subtotal * taxRate   / 100);
+    System.out.println("subtotal : " + subtotal);
     System.out.println("totalPriceVAT : " + totalPriceVAT);
 
     // Opgave 2 Split restaurant bill
+    double foodPrice = 450;
+    double drinksPrice = 120;
+    double tip = 50;
+    int numberAfPerson = 4;
+    double invoice = foodPrice + drinksPrice + tip;
+    double totalPrPerson = invoice / numberAfPerson;
+    System.out.println("Total per person : " + totalPrPerson);
 
+    // Opgave 3 Discount calculation
+    double totalPrice = 300;
+    double discountRate = 20;
+    double discountedPrice = totalPrice - (totalPrice * discountRate / 100);
+    double taxPercent = 25;
+    double finalAmount = discountedPrice + (discountedPrice * taxPercent / 100);
+    System.out.println("Oprindelige Pris : " + totalPrice);
+    System.out.println("Prisen Efter Rabat : " + discountedPrice);
+    System.out.println("Endelige Pris : " + finalAmount);
 
 }
 
+void Operators(){
 
+    // Opgave 4 Party planning
+
+
+}
 
