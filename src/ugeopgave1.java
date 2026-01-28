cbf6cc55cbddff299b0b01217e4b189faf1179a3
fgoc <feyzullahgoctr@gmail.com>
2026-01-28 16:22:59 +0300

    First commit

--- src/ugeopgave1.java
+++ src/ugeopgave1.java
@@ -1,2 +1,26 @@
-public class ugeopgave1 {
+
+void main (){
+    VariableBasics();
+
 }
+
+
+void VariableBasics() {
+
+    // Opgave 1 Shopping cart
+    double firstPrice = 25.50;
+    double secondPrice = 15;
+    double thirdPrice = 30;
+    double subtotalen = firstPrice + secondPrice + thirdPrice;
+    double momsprocenten = 25;
+    double totalPriceVAT = subtotalen + (subtotalen * momsprocenten / 100);
+    System.out.println("subtotalen : " + subtotalen);
+    System.out.println("totalPriceVAT : " + totalPriceVAT);
+
+    // Opgave 2 Split restaurant bill
+
+
+}
+
+
+
