0a9a5603ca43786df4cf353460469ea176a3bf2a
fgoc <feyzullahgoctr@gmail.com>
2026-01-29 12:57:32 +0300

    First commit

--- src/ugeopgave1.java
+++ src/ugeopgave1.java
@@ -23,9 +23,9 @@ void VariableBasics() {
     double foodPrice = 450;
     double drinksPrice = 120;
     double tip = 50;
-    int numberAfPerson = 4;
+    int numberAfPeople = 4;
     double invoice = foodPrice + drinksPrice + tip;
-    double totalPrPerson = invoice / numberAfPerson;
+    double totalPrPerson = invoice / numberAfPeople;
     System.out.println("Total per person : " + totalPrPerson);
 
     // Opgave 3 Discount calculation
